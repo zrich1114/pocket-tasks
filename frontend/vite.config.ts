78ade718400acc00fa87cb7028ed7953db058018
Zach Richardson <zachrichardson1114@gmail.com>
2025-10-25 16:35:27 -0600

    chore: configured cors on frontend and added env file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5174",
+        changeOrigin: true,
+      },
+    },
+  },
   preview: { port: 5173 },
 });
